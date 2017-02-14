411639a0a0fca2812d614efb19cc12774efc181c
DragonDragoon <wwoodar1@uncc.edu>
2017-02-14 10:05:35 -0500

    0.0.3: Add support for nicknames

--- js/server.js
+++ js/server.js
@@ -4,25 +4,43 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var default_name = "Anonymous";
+
+var users = {};
+
 app.get('/', function (req, res) {
   res.sendFile('index.html', { 'root': './' });
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
+  console.log('Client Connected (ID: ' + socket.id + ').');
 
-  io.emit('user connected');
+  users[socket.id] = {
+    name: default_name
+  };
+
+  socket.emit('enter username');
+
+  socket.on('username entered', function (_name) {
+    users[socket.id] = {
+      name: (_name !== '') ? _name : default_name
+    };
+
+    io.emit('user connected', users[socket.id].name);
+
+    console.log('Client Set Username (ID: ' + socket.id + ', Name: ' + users[socket.id].name + ').');
+  });
 
   socket.on('chat message', function (msg) {
-    io.emit('chat message', msg);
+    io.emit('chat message', users[socket.id].name, msg);
 
-    console.log('message: ' + msg);
+    console.log(users[socket.id].name + ": " + msg);
   });
 
   socket.on('disconnect', function () {
-    io.emit('user disconnected');
+    io.emit('user disconnected', users[socket.id].name);
 
-    console.log('user disconnected');
+    console.log('Client Disconnected (ID: ' + socket.id + ', Name: ' + users[socket.id].name + ').');
   });
 });
 
